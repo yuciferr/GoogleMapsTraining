1e4a2fcf17f6830c812e626c50899123e05bedb2
Yusuf Samed Çelik <ysfsmdc@gmail.com>
2023-03-13 16:30:34 +0300

    current location and select location was added

--- app/src/main/java/com/example/googlemapstraining/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapstraining/MapsActivity.kt
@@ -1,7 +1,14 @@
 package com.example.googlemapstraining
 
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +17,15 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.googlemapstraining.databinding.ActivityMapsBinding
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var  locationListener: LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +50,87 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapLongClickListener(listener)
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        /*
+        val ankara = LatLng(39.92, 32.84)
+        mMap.addMarker(MarkerOptions().position(ankara).title("Marker in Ankara"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ankara,15f))*/
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+
+        locationListener = object : LocationListener{
+            override fun onLocationChanged(p0: Location) {
+                mMap.clear()
+                val location = LatLng(p0.latitude,p0.longitude)
+                mMap.addMarker(MarkerOptions().position(location).title("You are Here"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15f))
+
+            }
+        }
+
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != android.content.pm.PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1)
+
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10f,locationListener)
+            val latest = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if(latest != null){
+                val latestLatLng = LatLng(latest.latitude,latest.longitude)
+                mMap.clear()
+                mMap.addMarker(MarkerOptions().position(latestLatLng).title("Your last location"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latestLatLng,15f))
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode==1){
+            if(grantResults.size>0){
+                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10f,locationListener)
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    val listener = object : GoogleMap.OnMapLongClickListener {
+        override fun onMapLongClick(p0: LatLng) {
+            mMap.clear()
+
+            val geocoder = Geocoder(this@MapsActivity, Locale.getDefault())
+
+            if(p0 != null){
+
+                var address = ""
+                try {
+                    val addressList = geocoder.getFromLocation(p0.latitude,p0.longitude,1)
+                    if(addressList?.size!! > 0){
+
+                        if(addressList[0].thoroughfare != null) {
+                            address += addressList[0].thoroughfare
+                            if(addressList[0].subThoroughfare != null) {
+                                address += addressList[0].subThoroughfare
+
+                            }
+                        }
+                    }
+
+                } catch (e: Exception){
+                    e.printStackTrace()
+                }
+
+                mMap.addMarker(MarkerOptions().position(p0).title(address))
+            }
+
+        }
     }
 }
